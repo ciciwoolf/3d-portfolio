5bf01d3d57a3eb56816e260c18aef54b4c2d7ea9
Cici Woolf <christinemarie.woolf@gmail.com>
2025-05-28 09:31:28 -0500

    Is it working?

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
-
+import Hero from './sections/Hero.jsx'
 const App = () => {
     return (
-        <main className="min-h-screen bg-deep-blue">
+        <main className="min-h-screen bg-blue-50">
             <h1>App</h1>
+            <Hero />
         </main>
     )
 }
