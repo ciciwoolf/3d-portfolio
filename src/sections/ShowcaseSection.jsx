1d793e28ee1756b16e0a5403714b37954c305a0e
Cici Woolf <christinemarie.woolf@gmail.com>
2025-06-20 09:52:41 -0500

    Add text and link to art website

--- src/sections/ShowcaseSection.jsx
+++ src/sections/ShowcaseSection.jsx
@@ -7,11 +7,29 @@ const ShowcaseSection = () => {
                 <div className="showcaselayout">
                     <div className="first-project-wrapper">
                         <div className="image-wrapper">
-                            <img src="/images/art-website.png" alt="Artwork" />
+                            <a
+                                href="https://www.theartofciciwoolf.com"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                                className="block"
+                            >
+                                <img
+                                    src="https://res.cloudinary.com/dmezw1zxr/image/upload/f_auto,q_auto,w_800/v1750430107/Screenshot_2025-06-19_at_11.12.03_AM_akcbnm.png"
+                                    alt="Cici Woolf's Art Website"/>
+                            </a>
                         </div>
-
+                        <div className="text-content">
+                            <h2>
+                                The Art of Cici Woolf Website App
+                            </h2>
+                            <p className="text-white-50 md:text-xl">
+                                An app to display my artwork, built with Next.js, React.js, and Tailwind CSS.
+                            </p>
+                        </div>
                     </div>
+
                 </div>
+
             </div>
         </div>
     )
