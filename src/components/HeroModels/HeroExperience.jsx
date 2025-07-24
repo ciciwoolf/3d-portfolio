8bc03a41daf88bded1a450b84aa72ef8cbf25a3a
Cici Woolf <christinemarie.woolf@gmail.com>
2025-07-24 15:52:49 -0500

    Add background color to html

--- src/components/HeroModels/HeroExperience.jsx
+++ src/components/HeroModels/HeroExperience.jsx
@@ -1,6 +1,5 @@
 import { OrbitControls } from "@react-three/drei";
 import { Canvas, useFrame } from "@react-three/fiber";
-import { EffectComposer, SelectiveBloom } from "@react-three/postprocessing";
 import { useMediaQuery } from "react-responsive";
 import { useRef } from "react";
 import { Room } from "./Room";
